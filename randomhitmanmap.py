802b3fa0a97f4003d1fd3a05db11e757f267891f
yarnie13 <64959619+yarnie13@users.noreply.github.com>
2020-12-12 09:53:55 +0000

    Add files via upload

---
+++ randomhitmanmap.py
@@ -0,0 +1,8 @@
+import random
+maps = ['World of Tomorrow', 'Club 27', 'Freedom Fighters', 'Situs Inversus', 'Hokkaido', 'Nightcall', 'The Finish Line', 'A Silver Tongue', 'Another Life' ,'A Better Pill', 'The Ark Society', 'Golden Handshake', 'The Last Resort']
+
+a = 1
+while a == 1:
+    print(random.choice(maps))
+    print("")
+    input()
